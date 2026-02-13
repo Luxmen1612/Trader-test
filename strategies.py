2b5852afddd2caa9f309d820c548174ae028cc72
david <raths_david@yahoo.de>
2026-02-13 18:21:16 +0100

    version push

--- strategies.py
+++ strategies.py
@@ -10,7 +10,8 @@ class MomentumBenchmark:
         self.prices = yf.download(tickers=tickers, period="max").iloc[-2520:].dropna(axis=1)
         self.quarterly_prices = self.prices.resample("M").last()
         self.returns = self.prices.Close.pct_change().dropna()
-        self.quarterly_dates = self.quarterly_prices.index
+        self.quarterly_dates = self.quarterly_prices.index[:-1]
+        volume_data = self.prices.Volume
         self.tickers = {}
         self.reference_portfolio_momentum = pd.Series()
         self.reference_portfolio_low_momentum = pd.Series()
@@ -19,43 +20,42 @@ class MomentumBenchmark:
         self.long = {}
         self.short = {}
         self.ls = {}
+        self.X = {}
 
         for i in self.quarterly_dates:
             print(i)
             if list(self.quarterly_dates).index(i) != 0:
-                self.tickers[i] = self.get_ls_tickers(self.returns[i - datetime.timedelta(20): i], size = 50)  ##### This gives me the portfolio of long and short tickers
+                self.tickers[i] = self.get_ls_tickers(self.returns[i - datetime.timedelta(30): i], size = 100)  ##### This gives me the portfolio of long and short tickers
 
         for k,v in self.tickers.items():
-            X = self.get_portfolio_value(k,v)
+            if k != self.quarterly_dates[-1]:
+                X = self.get_portfolio_value(k,v, volume_data)
 
-
-        X = pd.DataFrame.from_dict(X)
+        X = pd.DataFrame.from_dict(self.X).transpose()
         X['state reset'] = (X['state'] == 0).cumsum()
         X['state_length'] = df.groupby('reset')['state'].cumsum()
-
-        #spread = (self.momentum_portfolio_engineered - self.low_momentum_portfolio_engineered).resample("M").sum()
-        #monthly_liquidity = yf.download(tickers=tickers, period="max").Volume.iloc[-2520:].dropna(axis=1).resample("M").sum()
 
         sp = yf.download(tickers="^GSPC", period="max").Close.iloc[-2520:].dropna(axis=1).pct_change().dropna().resample("Q").sum().cumsum()
 
 
-    def get_portfolio_value(self, k,v):
+    def get_portfolio_value(self, k,v, volume_data):
 
         X = {} ##explanatory variables
-        if list(self.quarterly_dates).index(k) < len(self.quarterly_dates)-1:
-            k_idx = list(self.quarterly_dates).index(k)
-            next_idx = k_idx + 1
-            next_date = self.quarterly_dates[next_idx]
+        #if list(self.quarterly_dates).index(k) < len(self.quarterly_dates)-1:
+        k_idx = list(self.quarterly_dates).index(k)
+        next_idx = k_idx + 1
+        next_date = self.quarterly_dates[next_idx]
 
-            long_portfolio = self.returns[self.tickers[k][0].index].loc[k:next_date].dropna().mean(axis = 1)
-            self.momentum_portfolio_engineered = pd.concat([self.momentum_portfolio_engineered, long_portfolio], axis = 0)
+        long_portfolio = self.returns[self.tickers[k][0].index].loc[k:next_date].dropna().mean(axis = 1)
+        self.momentum_portfolio_engineered = pd.concat([self.momentum_portfolio_engineered, long_portfolio], axis = 0)
 
-            short_portfolio = self.returns[self.tickers[k][1].index].loc[k:next_date].dropna().mean(axis = 1)
-            self.low_momentum_portfolio_engineered = pd.concat([self.low_momentum_portfolio_engineered, short_portfolio], axis=0)
+        short_portfolio = self.returns[self.tickers[k][1].index].loc[k:next_date].dropna().mean(axis = 1)
+        self.low_momentum_portfolio_engineered = pd.concat([self.low_momentum_portfolio_engineered, short_portfolio], axis=0)
 
-            X[k] = {"vol_market":self.returns.loc[k:next_date].dropna().std().max(),
+        self.X[k] = {"vol_market":self.returns.loc[k:next_date].dropna().std().max(),
                     "vol_spread":long_portfolio.std() - short_portfolio.std(),
-                    "liquidity_spread":self.data[self.tickers[k[0]]].Volume.loc[k:next_date].dropna().mean(axis = 1) - self.data[self.tickers[k[1]]].Volume.loc[k:next_date].dropna().mean(axis = 1),
+                    #"liquidity_spread":self.prices[(self.tickers[k[0]]].Volume.loc[k:next_date].dropna().mean(axis = 1) - self.prices[self.tickers[k[1]]].Volume.loc[k:next_date].dropna().mean(axis = 1),
+                    "momentum_liquidity": volume_data[self.tickers[k][0].index].loc[k:next_date].dropna().sum(axis = 1).sum(),
                     "state":1 if long_portfolio.sum() > short_portfolio.sum() else 0}
 
         return X
